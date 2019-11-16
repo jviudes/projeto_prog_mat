6651025903a1354b28e65f10d6405c5890e1617f
johnny <jhonathan@sflabs.com.br>
2019-11-16 10:32:00 -0300

    updade README

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
-# Projeto da disciplina de Programação Matemática USP São Carlos
+## Projeto da disciplina de Programação Matemática
+##### ICMC – USP São Carlos
 
-#### install julia
-    - sudo apt-get install julia
 
-#### run project 
-    -   julia src/app.jl
-  
-
-## Problema
-* Um vendedor precisa visitar seus clientes em *n* cidades.
+* ## Problema
+  * O modelo observado em questão consiste no problema do caixeiro viajante, onde um vendedor precisa visitar seus clientes em *n* cidades.
   * Ele parte de uma cidade e deve retornar a essa cidade após visitar
 todas as outras.
   * Cada cidade deve ser visitada uma única vez.
-
 
 * ## Formulação
   * ### Objetivo
@@ -24,4 +18,32 @@ todas as outras.
     > ![Restriçoes](imgs/sa2.svg)
 
     > ![Restriçoes](imgs/sa3.svg)
+  
+  * ### Variáveis
+    * cityPos[ i ] -> posição (x,y) da cidade i
+    * nCities -> numero total de cidades
+    * dist[ i ][ j ]-> matriz de distancia euclidiana das cidades
+    * visit[ i ][ j ]-> matriz binária indicando se uma cidade foi visitada ou não
+    * u[ i ] -> numero de visitas realizadas até o nó i
+
+  * ### Solução
+    * O problema foi resolvido utilizando o solver Cbc da linguagem *julia*, para se reduzir a complexidade e por sua vez eliminar subrotas foi implementado o método Miller, Tucker e Zemlin (MTZ) O(n²). 
+    * O método  MTZ garante que dados dois nós i e j:
+      1. Se i precede j imediatamente: uj ≥ ui + 1
+      2. Caso contrário, esta restrição é desligada
+    * Dada as restrições anteriores obtivemos a seguinte formulação: 
+      * ![Restriçoes](imgs/sa2.svg)
+      * ![Restriçoes](imgs/sa3.svg)
+    
+     
+    
+
+
+* ## Setup
+
+  #### install julia
+      - sudo apt-get install julia
+
+  #### run project 
+      - julia src/app.jl
   
--- src/tsp.jl
+++ src/tsp.jl
@@ -8,20 +8,19 @@ cityPos[2] = (527,566)
 cityPos[3] = (435,603)
 cityPos[4] = (386,660)
 cityPos[5] = (346,692)
-# cityPos[6] = (431,730)
-#cityPos[7] = (419,818)
-#cityPos[8] = (347,520)
-#cityPos[9] = (332,330)
-#cityPos[10] = (165,374)
-#cityPos[11] = (196,198)
-#cityPos[12] = (187,108)
-#cityPos[13] = (210,63)
+cityPos[6] = (431,730)
+cityPos[7] = (419,818)
+cityPos[8] = (347,520)
+cityPos[9] = (332,330)
+cityPos[10] = (165,374)
+cityPos[11] = (196,198)
+cityPos[12] = (187,108)
+cityPos[13] = (210,63)
 
 nCities = length(cityPos)
 
-# pyplot() # plotagem do grafico
 # VARIAVEIS
-posX, posY = [], [] #posicao dos vertices para o grafico
+# posX, posY = [], [] #posicao dos vertices para o grafico
 dist = zeros(nCities,nCities)
 for i in sort(collect(keys(cityPos))) #calcula distancia euclidiana
     posI = cityPos[i]
@@ -29,14 +28,13 @@ for i in sort(collect(keys(cityPos))) #calcula distancia euclidiana
         posJ = cityPos[j]
         dist[i,j]=((posI[1]-posJ[1])^2+(posI[2]-posJ[2])^2)^0.5
     end
-    #append!(posX,posI[1])
-    #append!(posY,posI[2])
+    # append!(posX,posI[1])
+    # append!(posY,posI[2])
 end
 @variable(tsp, visit[i=1:nCities, j=1:nCities; i!=j], Bin) #matriz de arestas visitadas (cidades)
 @variable(tsp, u[1:nCities], Int) #declaracao do vetor u
 
-#definicao e plotagem do grafico
-#graphplot(1:nCities, 1:nCities, names=1:nCities,x=posX,y=posY,fontsize=10,m=:white,l=:black)
+# graphplot(1:nCities, 1:nCities, names=1:nCities,x=posX,y=posY,fontsize=10,m=:white,l=:black)
 
 #FUNCAO OBJETIVO
 @objective(tsp, Min, sum(dist[i,j] * visit[i,j] for i in 1:nCities, j in 1:nCities if i!=j) )
@@ -45,18 +43,14 @@ end
 for i in 1:nCities # Garantir saida de todos os vertices
     @constraint(tsp, sum(visit[i,j] for j in 1:nCities if i!=j ) == 1)
 end
-
 for j in 2:nCities # Garantir entrada em todos os vertices
     @constraint(tsp, sum(visit[i,j] for i in 1:nCities if i!=j ) == 1)
 end
 
-# restricao MTZ
-@constraint(tsp, u[1] == 0) 
-
+@constraint(tsp, u[1] == 0)  # restricao MTZ
 for i in 1:nCities
     @constraint(tsp, u[i] >= 0) 
 end
-
 for i in 1:nCities
     for j in 2:nCities
         if i!=j
@@ -65,12 +59,14 @@ for i in 1:nCities
     end
 end
 
+
 optimize!(tsp)
 if termination_status(tsp) == MOI.OPTIMAL
     println("Solucao otima encontrada \n\nVetor u : \n")
     for i in 1:nCities
         println(value(u[i]))
     end
+    
     println("\nDistancia total: ")
     println(objective_value(tsp))
 else println("Solucao otima nao encontrada")
